c3518136df300bfe492edc8c74153e86286f87f9
Félix Prado <felpragar@alum.us.es>
2015-07-21 01:24:36 +0200

    Primera pregunta

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var routes = require('./routes/index.js');
 
 var app = express();
 
@@ -14,7 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(__dirname + '/public/favicon.ico'));
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+ var express = require('express');
+ var router = express.Router();
+ 
+var quizController = require('../controllers/quiz_controller.js');
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
-});
+ /* GET home page. */
+ router.get('/', function(req, res) {
+   res.render('index', { title: 'Quiz' });
+ });
+ 
+/* Quiz: question */
+router.get('/quizes/question', quizController.question);
 
-module.exports = router;
+/* Quiz: answer */
+router.get('/quizes/answer', quizController.answer);
+
+ module.exports = router;
