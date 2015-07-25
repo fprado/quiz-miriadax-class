dde97866bd1f233026b4ee3b0a573717497ad8c3
Félix Prado <felpragar@alum.us.es>
2015-07-25 18:06:09 +0200

    Añadiendo sequelize-js y SQLite

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,4 +1,6 @@
 
+var models = require('../models/models.js');
+
 // GET /quizes/question
 exports.question = function( req, res ) {
 
@@ -9,11 +11,19 @@ exports.question = function( req, res ) {
 // GET /quizes/answer
 exports.answer = function( req, res ) {
 
-  if( req.query.respuesta === "Roma" ) {
-    res.render('quizes/answer', { respuesta: 'Correcto' } );
-  }
-  else {
-    res.render('quizes/answer', { respuesta: 'Incorrecto' } );
-  }
+    models.Quiz.findAll().success(function(quiz) {
+        if (req.query.respuesta === quiz[0].respuesta)
+        {
+            res.render('quizes/answer', {
+                respuesta: 'Correcto'
+            });
+        }
+        else
+        {
+            res.render('quizes/answer', {
+                respuesta: 'Incorrecto'
+            });
+        }
+    })
 
 }
